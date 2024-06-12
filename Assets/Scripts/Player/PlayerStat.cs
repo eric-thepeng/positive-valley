e6469a150171211d2b3f61ab04874622039d4a7c
Peng Guo <errrrrricg@gmail.com>
2024-06-12 22:26:00 +0800

    stat ui and animation

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -27,11 +27,23 @@ public class PlayerStat : MonoBehaviour
 
     public static int GetLevelUpExpRequirement()
     {
-        return level.GetValue() * 100;
+        return level.GetValue() * 50;
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            money.ChangeValue(10);
+        }else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            money.ChangeValue(100);
+        }else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            experience.ChangeValue(10);
+        }else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            experience.ChangeValue(100);
+        }
     }
 }
--- Assets/Scripts/Screen Space UI/UI_PlayerStatDisplayer.cs
+++ Assets/Scripts/Screen Space UI/UI_PlayerStatDisplayer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.LowLevel;
@@ -11,50 +12,81 @@ public class UI_PlayerStatDisplayer : MonoBehaviour
     [Header("Money Dependencies")] 
     [SerializeField] private GameObject moneyGameObject;
     [SerializeField] private bool displayMoney = true;
+    [SerializeField] private bool moneyIncreaseAnimation = true;
     [SerializeField] private TMP_Text moneyAmountTMPT;
     
     [Header("Experience Dependencies")]
     [SerializeField] private GameObject experienceGameObject;
     [SerializeField] private bool displayExperience = true;
+    [SerializeField] private bool experienceIncreaseAnimation = true;
     [SerializeField] private TMP_Text experienceAmountTMPT;
     
     [Header("Experience Dependencies")]
     [SerializeField] private GameObject levelGameObject;
     [SerializeField] private bool displayLevel = true;
+    [SerializeField] private bool levelIncreaseAnimation = true;
     [SerializeField] private TMP_Text levelAmountTMPT;
 
     private void OnEnable()
     {
-        PlayerStat.money.SubscribeChangeValue(RefreshMoneyDisplay);
+        // MONEY 
+        PlayerStat.money.SubscribeChangeDelta(RefreshMoneyDisplay);
         RefreshMoneyDisplay(0);
-        
-        PlayerStat.experience.SubscribeChangeValue(RefreshExperienceDisplay);
+        if (displayMoney) {moneyGameObject.SetActive(true); }
+        else{moneyGameObject.SetActive(false); }
+
+        // EXPERIENCE
+        PlayerStat.experience.SubscribeChangeDelta(RefreshExperienceDisplay);
         RefreshExperienceDisplay(0);
+        if (displayExperience) {experienceGameObject.SetActive(true); }
+        else{experienceGameObject.SetActive(false); }
         
-        PlayerStat.level.SubscribeChangeValue(RefreshLevelDisplay);
+        // LEVEL
+        PlayerStat.level.SubscribeChangeDelta(RefreshLevelDisplay);
         RefreshLevelDisplay(0);
+        if (displayLevel) {levelGameObject.SetActive(true); }
+        else{levelGameObject.SetActive(false); }
     }
 
     private void OnDisable()
     {
-        PlayerStat.money.UnsubscribeChangeValue(RefreshMoneyDisplay);
-        PlayerStat.experience.UnsubscribeChangeValue(RefreshExperienceDisplay);
-        PlayerStat.level.UnsubscribeChangeValue(RefreshLevelDisplay);
+        PlayerStat.money.UnsubscribeChangeDelta(RefreshMoneyDisplay);
+        PlayerStat.experience.UnsubscribeChangeDelta(RefreshExperienceDisplay);
+        PlayerStat.level.UnsubscribeChangeDelta(RefreshLevelDisplay);
     }
 
-    private void RefreshMoneyDisplay(int newValue)
+    private void RefreshMoneyDisplay(int delta)
     {
         moneyAmountTMPT.text = PlayerStat.money.GetValue() + "";
+        if (delta > 0 && moneyIncreaseAnimation)
+        {
+            moneyAmountTMPT.gameObject.transform.localScale = new Vector3(1, 1, 1);
+            moneyAmountTMPT.gameObject.transform.DOKill();
+            moneyAmountTMPT.gameObject.transform.DOPunchScale(new Vector3(.5f, .5f, .5f), 0.5f,3,0f);
+        }
     }
+    
 
-    private void RefreshExperienceDisplay(int newValue)
+    private void RefreshExperienceDisplay(int delta)
     {
         experienceAmountTMPT.text = PlayerStat.experience.GetValue() + "/" + PlayerStat.GetLevelUpExpRequirement();
+        if (delta > 0 && experienceIncreaseAnimation)
+        {
+            experienceAmountTMPT.gameObject.transform.localScale = new Vector3(1, 1, 1);
+            experienceAmountTMPT.gameObject.transform.DOKill();
+            experienceAmountTMPT.gameObject.transform.DOPunchScale(new Vector3(.5f, .5f, .5f), 0.5f,3,0f);
+        }
     }
 
-    private void RefreshLevelDisplay(int newValue)
+    private void RefreshLevelDisplay(int delta)
     {
         levelAmountTMPT.text = PlayerStat.level.GetValue() + "";
-        RefreshExperienceDisplay(0);
+        RefreshExperienceDisplay(delta);
+        if (delta > 0 && levelIncreaseAnimation)
+        {
+            levelAmountTMPT.gameObject.transform.localScale = new Vector3(1, 1, 1);
+            levelAmountTMPT.gameObject.transform.DOKill();
+            levelAmountTMPT.gameObject.transform.DOPunchScale(new Vector3(.5f, .5f, .5f), 0.5f,3,0f);
+        }
     }
 }
